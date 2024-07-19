9d0de61c3964bf94f2c14e1819be9a76b72ac67e
Dmytro Anikin <dmytro.anikin@checkout.com>
2024-07-19 11:20:06 +0200

    Init

---
+++ app.js
@@ -0,0 +1,87 @@
+/* global CheckoutWebComponents */
+(async () => {
+    // Insert your public key here
+    const PUBLIC_KEY = "pk_sbox_guri7tp655hvceb3qaglozm7gee";
+  
+    const response = await fetch("https://api.sandbox.checkout.com/payment-sessions", { 
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer sk_sbox_odyoauybjpcfbkzmgsnnbiub4mj`,
+        },
+        body: JSON.stringify({
+            processing_channel_id: "pc_w2njpb6jbjjujgcz5dgzxdn5mm",
+            currency: "EUR",
+            amount: 1212,
+            reference: `ORD-2234`,
+            "3ds": {
+              enabled: true, 
+            },
+            billing: {
+              address: {
+                country: "NL", // GB
+              },
+            },
+            success_url: "https://checkout.checkout.test.success",
+            failure_url: "https://checkout.checkout.test.failure",
+        })
+    }); // Order
+    const paymentSession = await response.json();
+  
+    if (!response.ok) {
+      console.error("Error creating payment session", paymentSession);
+      return;
+    }
+  
+    const checkout = await CheckoutWebComponents({
+      publicKey: PUBLIC_KEY,
+      environment: "sandbox",
+      locale: "fr-FR", // en-GB
+      paymentSession,
+      onReady: () => {
+        console.log("onReady");
+      },
+      onPaymentCompleted: (_component, paymentResponse) => {
+        console.log("Create Payment with PaymentId: ", paymentResponse.id);
+      },
+      onChange: (component) => {
+        console.log(
+          `onChange() -> isValid: "${component.isValid()}" for "${
+            component.type
+          }"`,
+        );
+      },
+      onError: (component, error) => {
+        console.log("onError", error, "Component", component.type);
+      },
+    });
+  
+    const flowComponent = checkout.create("flow");
+  
+    flowComponent.mount(document.getElementById("flow-container"));
+  })();
+  
+  function triggerToast(id) {
+    var element = document.getElementById(id);
+    element.classList.add("show");
+  
+    setTimeout(function () {
+      element.classList.remove("show");
+    }, 5000);
+  }
+  
+  const urlParams = new URLSearchParams(window.location.search);
+  const paymentStatus = urlParams.get("status");
+  const paymentId = urlParams.get("cko-payment-id");
+  
+  if (paymentStatus === "succeeded") {
+    triggerToast("successToast");
+  }
+  
+  if (paymentStatus === "failed") {
+    triggerToast("failedToast");
+  }
+  
+  if (paymentId) {
+    console.log("Create Payment with PaymentId: ", paymentId);
+  }
