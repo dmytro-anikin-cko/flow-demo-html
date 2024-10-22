41768b22790f6c60a18ad0aace2cbf1822fc0771
Dmytro Anikin <dmytro.anikin@checkout.com>
2024-10-22 18:24:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,54 +2,39 @@
 (async () => {
   const PUBLIC_KEY = "pk_sbox_guri7tp655hvceb3qaglozm7gee";
 
-  let paymentSession;
-
-  // Check if paymentSession is stored in localStorage
-  const savedPaymentSession = localStorage.getItem("paymentSession");
-
-  if (savedPaymentSession) {
-    // If there's a stored session, use it
-    paymentSession = JSON.parse(savedPaymentSession);
-    console.log("Loaded paymentSession from localStorage:", paymentSession);
-  } else {
-    // If no session in localStorage, create a new payment session
-    const response = await fetch(
-      "https://api.sandbox.checkout.com/payment-sessions",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer sk_sbox_odyoauybjpcfbkzmgsnnbiub4mj`,
+  // If no session in localStorage, create a new payment session
+  const response = await fetch(
+    "https://api.sandbox.checkout.com/payment-sessions",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer sk_sbox_odyoauybjpcfbkzmgsnnbiub4mj`,
+      },
+      body: JSON.stringify({
+        processing_channel_id: "pc_w2njpb6jbjjujgcz5dgzxdn5mm",
+        currency: "EUR",
+        amount: 1345,
+        reference: `ORD-2234`,
+        "3ds": {
+          enabled: true,
         },
-        body: JSON.stringify({
-          processing_channel_id: "pc_w2njpb6jbjjujgcz5dgzxdn5mm",
-          currency: "EUR",
-          amount: 1345,
-          reference: `ORD-2234`,
-          "3ds": {
-            enabled: true,
+        billing: {
+          address: {
+            country: "GB", // GB
           },
-          billing: {
-            address: {
-              country: "GB", // GB
-            },
-          },
-          success_url: "https://checkout.checkout.test.success",
-          failure_url: "https://checkout.checkout.test.failure",
-        }),
-      }
-    );
-
-    paymentSession = await response.json();
-
-    if (!response.ok) {
-      console.error("Error creating payment session", paymentSession);
-      return;
+        },
+        success_url: "https://checkout.checkout.test.success",
+        failure_url: "https://checkout.checkout.test.failure",
+      }),
     }
+  );
 
-    // Save the paymentSession to localStorage
-    localStorage.setItem("paymentSession", JSON.stringify(paymentSession));
-    console.log("Saved paymentSession to localStorage:", paymentSession);
+  let paymentSession = await response.json();
+
+  if (!response.ok) {
+    console.error("Error creating payment session", paymentSession);
+    return;
   }
 
   /* 
@@ -76,7 +61,7 @@
     onSubmit: (_self) => {
       // Show loading overlay to prevent further user interaction
       console.log(_self);
-      debugger;
+      // debugger;
     },
     onError: (component, error) => {
       console.log("onError", error, "Component", component.type);
